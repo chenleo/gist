639d678581c51af59107c6c1f2851fdc67100462
maverickmw <maverickcc@gmail.com>
2013-09-19 15:26:32 +0200

    add filtered result of FRET

--- triplet_proximity.m
+++ triplet_proximity.m
@@ -1,12 +1,12 @@
 % Test simulation for the contribution of spFRET from Triplet Event.
 
 % Set Proximity FRET Ratio:
-proximity = 0.5;
+proximity = 0.6;
 
 % Set Triplet effect:
 % 0 for no triplet, 1 for all triplet;
-D_tri = 0;
-A_tri = 0;
+D_tri = 1;
+A_tri = 1;
 
 % Set wave packet:
 N = 1000;   % Number of sample time
@@ -15,6 +15,8 @@ t = linspace(-1,1,N+1);
 t = t(1:N); % Last point == First point
 
 % Set wave packet of donor:
+%% plot initial data;
+figure(1);
 a = subplot(2,2,1);
 D = 1;
 D_pack = D * exp(- t.^2/W);
@@ -50,7 +52,33 @@ plot(A_pack./(D_pack+A_pack),'o')
 
 % plot FRET value point(or hist) with triplet:
 subplot(2,2,4);
-
-
-hist(A_trip./(D_trip+A_trip));
+hist(A_trip./(D_trip+A_trip),100);
 % plot(A_trip./(D_trip+A_trip),'o')
+
+%% Plot proximity ratio after filtered 
+
+figure(2);
+filter_width = 2; % half width in frequency domain. If set to 1, only mean value will be left.
+A_fft = fft(A_trip);
+D_fft = fft(D_trip);
+
+fft_filter = zeros(1,N);
+fft_filter(1:filter_width) = 1;
+fft_filter(N-filter_width+2:N) = 1;
+
+% filter 
+D_fft = D_fft.*fft_filter;
+A_fft = A_fft.*fft_filter;
+
+D_ifft = ifft(D_fft);
+A_ifft = ifft(A_fft);
+subplot(2,1,1);
+result = A_ifft./(A_ifft + D_ifft);
+
+plot(result(200:800),'o');
+subplot(2,1,2);
+hist(result(200:800),20,[0 1]);
+
+
+
+
